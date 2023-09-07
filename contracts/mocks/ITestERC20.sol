02b0d1ac9f2819fdb8c04b6a668fe836f2a342bb
rohan0042 <tony@decake.xyz>
2023-09-07 20:28:18 +0900

    init

---
+++ contracts/mocks/ITestERC20.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.12;
+
+import "../interfaces/IERC20.sol";
+
+interface ITestERC20 is IERC20Uniswap {
+    function mint(address to, uint amount) external;
+    function burn(uint amount) external;
+}
