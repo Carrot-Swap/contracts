e98da6030bf4dcb3d50719f5c3fc559cd510d339
rohan0042 <tony@decake.xyz>
2023-09-07 20:28:41 +0900

    setup hardhat

---
+++ hardhat.config.ts
@@ -0,0 +1,46 @@
+import { HardhatUserConfig } from "hardhat/config";
+
+import "@openzeppelin/hardhat-upgrades";
+import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+import "@nomiclabs/hardhat-ethers";
+
+require("hardhat-contract-sizer");
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.6.12",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+  networks: {
+    hardhat: {
+      forking: {
+        url: "https://evm.ngd.network:32331", // testnet node
+      },
+      accounts: {
+        mnemonic: "test test test test test test test test test test test junk",
+        initialIndex: 0,
+        accountsBalance: "10000000000000000000000000",
+      },
+    },
+    neoevm: {
+      url: "https://evm.ngd.network:32331",
+      chainId: 2970385,
+      accounts: require("./secrets.json").privateKey,
+      gas: 30000001,
+    },
+  },
+};
+
+export default config;
