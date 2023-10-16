9894d80fca95cf0f8d07709b41df7c841252ce6b
rohan0042 <143691034+rohan0042@users.noreply.github.com>
2023-10-16 11:33:31 +0900

    add faucet contract (#4)
    Co-authored-by: rohan0042 <tony@decake.xyz>

---
+++ packages/faucet/hardhat.config.ts
@@ -0,0 +1,40 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "hardhat-deploy";
+import "@openzeppelin/hardhat-upgrades";
+import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+import "@nomiclabs/hardhat-ethers";
+import dotenv from "dotenv";
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.9",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+  networks: {
+    neo_evm: {
+      url: "https://evm.ngd.network:32331",
+      chainId: 2970385,
+      accounts: require("./secrets.json").privateKey,
+      gas: 30000001,
+      saveDeployments: true,
+    },
+  },
+  namedAccounts: {
+    deployer: 0,
+  },
+};
+
+export default config;
