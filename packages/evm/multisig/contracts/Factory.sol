a9243107905483d944ffb04c4afbc8b344ec5941
rohan0042 <dapp.carrot.finance@gmail.com>
2023-11-14 17:56:58 +0900

    init

---
+++ packages/evm/multisig/contracts/Factory.sol
@@ -0,0 +1,35 @@
+pragma solidity ^0.8.9;
+
+contract Factory {
+    /*
+     *  Events
+     */
+    event ContractInstantiation(address sender, address instantiation);
+
+    /*
+     *  Storage
+     */
+    mapping(address => bool) public isInstantiation;
+    mapping(address => address[]) public instantiations;
+
+    /*
+     * Public functions
+     */
+    /// @dev Returns number of instantiations by creator.
+    /// @param creator Contract creator.
+    /// @return Returns number of instantiations by creator.
+    function getInstantiationCount(address creator) public constant returns (uint) {
+        return instantiations[creator].length;
+    }
+
+    /*
+     * Internal functions
+     */
+    /// @dev Registers contract in factory registry.
+    /// @param instantiation Address of contract instantiation.
+    function register(address instantiation) internal {
+        isInstantiation[instantiation] = true;
+        instantiations[msg.sender].push(instantiation);
+        ContractInstantiation(msg.sender, instantiation);
+    }
+}
